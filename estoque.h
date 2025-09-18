29204022d8c35e2cf8a345411098201b158b376f
cacece <120501973+cacece@users.noreply.github.com>
2025-09-18 19:54:59 -0300

    Update estoque.h
    comentado

--- estoque.h
+++ estoque.h
@@ -1,21 +1,21 @@
 #ifndef ESTOQUE_H
 #define ESTOQUE_H
-
+// Número máximo de tipos de ingredientes
 #define MAX_INGREDIENTES 12
-
+// Estrutura para representar um ingrediente
 typedef struct
 {
 	int id;
 	char nome[30];
 	float preco;
-	int quantidade;
+	int quantidade; // Quantidade em estoque
 } Ingrediente;
-
+// Estrutura para representar o estoque completo
 typedef struct
 {
 	Ingrediente ingredientes[MAX_INGREDIENTES];
 } Estoque;
-
+// Protótipos das funções definidas em estoque.c
 void inicializarEstoque(Estoque *estoque);
 void adicionarIngrediente(Estoque *estoque, int id, int quantidade);
 void exibirEstoque(Estoque *estoque);
