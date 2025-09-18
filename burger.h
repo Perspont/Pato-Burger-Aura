ca47c6bd0825eb2dac9bb7c5c7d50be502ed2310
cacece <120501973+cacece@users.noreply.github.com>
2025-09-18 19:55:45 -0300

    Update burger.h
    comentado

--- burger.h
+++ burger.h
@@ -1,16 +1,17 @@
 #ifndef BURGER_H
 #define BURGER_H
-
+// Número máximo de hambúrgueres no cardápio
 #define MAX_BURGERS 10
-
+// Estrutura para representar um hambúrguer
 typedef struct
 {
 	int id;
 	char nome[30];
 	float preco;
-	int ingredientes[10];
+	int ingredientes[10]; // Array com os IDs dos ingredientes necessários
 } Burger;
 
+// Protótipos das funções definidas em burger.c
 void inicializarCardapio(Burger *cardapio);
 void exibirCardapio(Burger *cardapio);
 
