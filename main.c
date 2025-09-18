e8f5e4eeeb29afb3d4e7f71e1b82e5fe1610f098
cacece <120501973+cacece@users.noreply.github.com>
2025-09-18 19:22:01 -0300

    Update main.c
    comentado

--- main.c
+++ main.c
@@ -9,6 +9,7 @@
 
 int main()
 {
+	// Declaração das principais estruturas de dados do jogo e inicialização dos dados do jogo
 	Burger cardapio[MAX_BURGERS];
 	inicializarCardapio(cardapio);
 	FilaPedidos pedidos;
@@ -18,31 +19,33 @@ int main()
 	Estado estado;
 	inicializarEstado(&estado);
 
+	// Loop principal do jogo, simula a passagem dos dias
 	for (int i = 0; i < estado.dia; i++) {
+		// Limpa a tela do console para uma nova exibição
 		#ifdef _WIN32
-			system("cls");
+			system("cls"); //Windows
 		#else
-			system("clear");
+			system("clear") //Linux  ou Mac
 		#endif
 
 
 		printf("Iniciando Dia %d\n", estado.dia);
 
-		sleep(1);
+		sleep(1); // Pausa a execução por 1 segundo
 
-		exibirCardapio(cardapio);
-
-		sleep(1);
-
-		exibirEstoque(&estoque);
+		exibirCardapio(cardapio); // Mostra o cardápio de hambúrgueres
 
 		sleep(1);
 
-		geraPedidos(&pedidos, estado.dia);
+		exibirEstoque(&estoque); // Mostra a quantidade atual de ingredientes
 
 		sleep(1);
 
-		imprimeFilaPedidos(&pedidos, cardapio);
+		geraPedidos(&pedidos, estado.dia); // Gera novos pedidos para o dia
+
+		sleep(1);
+
+		imprimeFilaPedidos(&pedidos, cardapio); // Exibe a fila de pedidos gerada
 	}
 
 		return 0;
