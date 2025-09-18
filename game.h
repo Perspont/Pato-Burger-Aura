4c255d23eda3a05e2c65f00cd83b3dc88e7f1fbc
cacece <120501973+cacece@users.noreply.github.com>
2025-09-18 19:56:36 -0300

    Update game.h
    comentado

--- game.h
+++ game.h
@@ -1,12 +1,12 @@
 #ifndef GAME_H
 #define GAME_H
-
+// Estrutura para guardar o estado atual do jogo
 typedef struct
 {
-	int dia;
-	float dinheiro;
+	int dia; // Dia atual
+	float dinheiro; // Dinheiro do player
 } Estado;
-
+// Protótipo da função definida em game.c
 void inicializarEstado(Estado *estado);
 
 #endif
