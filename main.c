f3f4ab19aeb54989136781936b72c6680ca49ec6
Perspont <pburitysil@gmail.com>
2025-09-21 16:00:45 -0300

    Adicionado funcionalidade de menu principal

--- main.c
+++ main.c
@@ -7,43 +7,10 @@
 #include <math.h>
 #include <time.h>
 
+
 int main()
 {
-	Burger cardapio[MAX_BURGERS];
-	inicializarCardapio(cardapio);
-	FilaPedidos pedidos;
-	inicializaFilaPedidos(&pedidos);
-	Estoque estoque;
-	inicializarEstoque(&estoque);
-	Estado estado;
-	inicializarEstado(&estado);
-
-	for (int i = 0; i < estado.dia; i++) {
-		#ifdef _WIN32
-			system("cls");
-		#else
-			system("clear");
-		#endif
-
-
-		printf("Iniciando Dia %d\n", estado.dia);
-
-		sleep(1);
-
-		exibirCardapio(cardapio);
-
-		sleep(1);
-
-		exibirEstoque(&estoque);
-
-		sleep(1);
-
-		geraPedidos(&pedidos, estado.dia);
-
-		sleep(1);
-
-		imprimeFilaPedidos(&pedidos, cardapio);
-	}
+		gameplayLoop();
 
 		return 0;
 }
