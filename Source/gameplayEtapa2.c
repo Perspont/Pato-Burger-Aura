2cf0d75c775bfb0aab267e4742bb6756eeec509b
Perspont <pburitysil@gmail.com>
2025-10-25 10:17:35 -0300

    Adicionei um comentario em gameplayEtapa2

--- Source/gameplayEtapa2.c
+++ Source/gameplayEtapa2.c
@@ -1,1 +1,3 @@
 #include "../Header/gameplayEtapa2.h"
+
+//Aqui teremos o jogo principal. Isto inclui montagem de hamburgueres, tela, recebimento de pedidos, e sistema de passagem de dias INTERCONECTADO com o sistema de loja.
